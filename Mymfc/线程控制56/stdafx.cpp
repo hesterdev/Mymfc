55629d7fecac254782150411ebc1b00d474a3234
hesterdev <940356013@qq.com>
2016-05-17 22:58:06 +0800

    change dir

--- Mymfc/线程控制56/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// Ïß³Ì¿ØÖÆ56.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
