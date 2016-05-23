766d576ea16a6383d10d582be93b07836e8e1674
hesterdev <940356013@qq.com>
2016-05-23 18:11:26 +0800

    a

--- Prj79/Source.cpp
+++ Prj79/Source.cpp
@@ -18,6 +18,88 @@ PTSTR BigNumToString(LONG lNum, PTSTR szBuf, DWORD chBufSize) {
 	return szBuf;
 }
 
+void ShowCPUInfo(HWND hWnd, WORD wProcessorArchitecture, WORD wProcessorLevel, WORD wPorcessorRevision) {
+	TCHAR szCPUArch[64] = TEXT("unknown");
+	TCHAR szCPULevel[64] = TEXT("unknown");
+	TCHAR szCPURev[64] = TEXT("unknown");
+
+
+	switch (wProcessorArchitecture)
+	{
+	case PROCESSOR_ARCHITECTURE_INTEL:
+		ua_tcscpy_s(szCPUArch, _countof(szCPUArch), TEXT("Intel"));
+		switch (wProcessorLevel)
+		{
+		case 3:
+		case 4:
+			StringCchPrintf(szCPULevel, _countof(szCPULevel), TEXT("80%c86"), wProcessorLevel + '0'/*数字转为字符*/);
+			StringCchPrintf(szCPURev, _countof(szCPURev), TEXT("%c%d"), HIBYTE(wPorcessorRevision) + TEXT('A'), LOBYTE(wPorcessorRevision));
+			break;
+		case 5://叫做奔腾  之前是386 486
+			ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium"));
+			StringCchPrintf(szCPURev, _countof(szCPURev), TEXT("Model %d, Stepping %d"), HIBYTE(wPorcessorRevision), LOBYTE(wPorcessorRevision));
+
+			break;
+		case 6:
+			switch (wProcessorLevel)
+			{
+			case 1:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium Pro"));
+				break;
+			case 3:
+			case 5:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium II"));
+				break;
+			case 6:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Celeron"));
+				break;
+			case 7:
+			case 8:
+			case 11:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium III"));
+				break;
+			case 9:
+			case 13:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium M"));
+				break;
+			case 10:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium Xeon"));//至强
+				break;
+			case 15:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Core 2 Duo"));
+				break;
+			default:
+				ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Unknown pentium"));
+
+				break;
+			}
+			StringCchPrintf(szCPURev, _countof(szCPURev), TEXT("Model %d, Stepping %d"),
+				HIBYTE(wPorcessorRevision), LOBYTE(wPorcessorRevision));
+			break;
+		case 15:// 奔腾4 截止到目前是最新的
+			ua_tcscpy_s(szCPULevel, _countof(szCPULevel), TEXT("Pentium 4"));
+			break;
+
+		default:
+			break;
+		}
+		break;
+	case PROCESSOR_ARCHITECTURE_IA64:
+		ua_tcscpy_s(szCPUArch, _countof(szCPUArch), TEXT("IA-64"));
+		break;
+	case PROCESSOR_ARCHITECTURE_AMD64:
+		ua_tcscpy_s(szCPUArch, _countof(szCPUArch), TEXT("AMD64"));
+		break;
+	case PROCESSOR_ARCHITECTURE_UNKNOWN:
+	default:
+		ua_tcscpy_s(szCPUArch, _countof(szCPUArch), TEXT("unknown"));
+		break;
+	}
+
+	SetDlgItemText(hWnd, IDC_PROCARCH, szCPUArch);
+	SetDlgItemText(hWnd, IDC_PROCLEVEL, szCPULevel);
+	SetDlgItemText(hWnd, IDC_PROREV, szCPURev);
+}
 
 INT_PTR CALLBACK MainDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	BOOL bRet = TRUE;
@@ -36,11 +118,15 @@ INT_PTR CALLBACK MainDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		StringCchPrintf(szBuf, _countof(szBuf), TEXT("%p"), sinf.lpMaximumApplicationAddress);
 		SetDlgItemText(hDlg, IDC_MAXADDR, szBuf);
 
-		StringCchPrintf(szBuf, _countof(szBuf),BigNumToString(sinf.dwAllocationGranularity,szBuf,_countof(szBuf)));
-		SetDlgItemText(hDlg,IDC_ALLOCGRAN, szBuf);
+		StringCchPrintf(szBuf, _countof(szBuf), BigNumToString(sinf.dwAllocationGranularity, szBuf, _countof(szBuf)));
+		SetDlgItemText(hDlg, IDC_ALLOCGRAN, szBuf);
 
+		SetDlgItemInt(hDlg, IDC_NUMOFPRO, sinf.dwNumberOfProcessors, FALSE);
 
+		StringCchPrintf(szBuf, _countof(szBuf), TEXT("0x%I64X", (__int64)sinf.dwActiveProcessorMask));
+		SetDlgItemText(hDlg, IDC_ACTIVEPROCMASK, szBuf);
 
+		ShowCPUInfo(hDlg, sinf.wProcessorArchitecture, sinf.wProcessorLevel, sinf.wProcessorRevision);
 		break;
 	case WM_CLOSE:
 		EndDialog(hDlg, 0);
---
+++ Project81/Source1.cpp
@@ -0,0 +1,49 @@
+#include<Windows.h>
+#include<strsafe.h>
+#include<Psapi.h>
+
+#pragma comment(lib,"psapi.lib")
+
+#include"resource.h"
+
+#define IDT_UPDATE 1
+
+
+INT_PTR CALLBACK MainDlgProc(HWND hDlg, WPARAM uMsg, WPARAM wParam, LPARAM lParam) {
+	BOOL bRet = TRUE;
+	switch (uMsg) {
+	case WM_INITDIALOG:
+
+		SetTimer(hDlg, IDT_UPDATE, 100, NULL);
+		SendMessage(hDlg, WM_TIMER, IDT_UPDATE, 0);
+	case WM_TIMER:
+	{
+		MEMORYSTATUSEX ms = { sizeof(ms) };
+		GlobalMemoryStatusEx(&ms);
+
+		TCHAR szData[512] = { 0 };
+		StringCchPrintf(szData, _countof(szData), TEXT("%d\n%I64d\n%I64d\n%I64d\n%I64d\n%I64d\n%I64d\n"), ms.dwMemoryLoad/*已经使用的内存的百分比*/, (__int64)ms.ullTotalPhys
+			, (__int64)ms.ullAvailPhys, (__int64)ms.ullTotalPageFile, (__int64)ms.ullAvailPageFile, (__int64)ms.ullTotalVirtual, (__int64)ms.ullAvailVirtual);
+
+		SetDlgItemText(hDlg, IDC_STATIC1, szData);
+
+		PROCESS_MEMORY_COUNTERS_EX pmc = { sizeof(pmc) };
+
+		GetProcessMemoryInfo(GetCurrentProcess(), (PPROCESS_MEMORY_COUNTERS)&pmc, sizeof(pmc));
+		StringCchPrintf(szData, _countof(szData), TEXT("%I64d K\n%I64d K"), (__int64)pmc.WorkingSetSize, (__int64)pmc.PrivateUsage);
+		SetDlgItemText(hDlg, IDC_STATIC2, szData);
+	}
+	break;
+	case WM_CLOSE:
+		EndDialog(hDlg, 0);
+		break;
+	default:
+		bRet = FALSE;
+		break;
+	}
+	return bRet;
+}
+
+int APIENTRY WinMain(HINSTANCE ins, HINSTANCE pins, LPTSTR lpCmdLine, int nCmdShow) {
+	DialogBox(ins, MAKEINTRESOURCE(IDD_DIALOG1), NULL, MainDlgProc);
+}
